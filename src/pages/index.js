fc4e483aecf6192460a1a37f132dbfad592293df
Lele Lew <lele@lelelew.com>
2018-08-29 17:20:39 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hey There!</h1>
+    <p>I'm Lele.</p>
+    <p>Welcome to my Gatsby site!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
